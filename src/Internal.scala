29d05184e8e41838b47750b1cbe3689fa670e520
Adam B <76700849+AdamrBeckwith@users.noreply.github.com>
2023-03-05 22:39:24 +0000

    Implementing Setup?

--- src/Internal.scala
+++ src/Internal.scala
@@ -7,13 +7,31 @@ import java.io.File
 
 trait PicoOrder
 
-case object Up extends PicoOrder
-case object Left extends PicoOrder
-case object Right extends PicoOrder
-case object Down extends PicoOrder
-case class Directive(String : name) extends PicoOrder
+trait State
 
-case class Detecting(PicoOrder : p1) extends PicoOrder
-case class And(PicoOrder : p1, PicoOrder : p2) extends PicoOrder
-case class Transitioning(PicoOrder : p1, PicoOrder : p2) extends PicoOrdercase 
-case class Via(PicoOrder : p1, PicoOrder : p2) extends PicoOrder
+case object Up extends State
+case object Left extends State
+case object Right extends State
+case object Down extends State 
+case object NotUp extends State
+case object NotLeft extends State
+case object NotRight extends State
+case object NotDown extends State
+case class Directive(name : String) extends State
+case class And(p1 : State, p2 : State) extends State
+
+
+case class Detecting(p1 : State ) extends PicoOrder
+case class Transitioning(initial : PicoOrder, result : PicoOrder) extends PicoOrder
+case class Via(direction : State, directive : State) extends PicoOrder
+
+def engage(orders : List[PicoOrder], directive: String) : Unit = {
+    var rules = List()
+    for(order <- orders) {
+        rules += convertToRule(order)
+    }
+}
+
+def convertToRule(order : PicoOrder) : Rule = {
+
+}
